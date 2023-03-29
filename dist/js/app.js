4e99e9897b85ab8626bb1e2310958637dc77e72a
DimaProhorenko <dima.study.web@gmail.com>
2023-03-29 22:48:17 +0300

    mobile nav works

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,13 +5,20 @@ const duration = 0.3;
 // Tweens
 
 // Burger tween
-const burgerTl = gsap.timeline()
+const burgerTl = gsap.timeline({ paused: true, reversed: true })
     .to('.burger__line--top', { yPercent: 250, duration})
     .to('.burger__line--mid', {opacity: 0, duration}, '<')
     .to('.burger__line--bot', {yPercent: -250, duration}, '<')
     .to('.burger', {rotation: 360, duration: duration})
     .to('.burger__line--top', {rotation: 45, transformOrigin: 'center center', duration})
     .to('.burger__line--bot', {rotation: -45, transformOrigin: 'center center', duration}, '<')
+
+
+// Nav Tween
+const navTl = gsap.timeline({ paused: true, reversed: true, ease: 'power4.inOut'})
+    .fromTo('.nav', {xPercent: 100, duration }, {xPercent: 0, duration})
+    .fromTo('.nav__overlay', {scaleX: 0, duration}, {scaleX: 1, duration})
+    .from('.nav__link', {y: -20, opacity: 0, duration, stagger: 0.1});
 
 burgerEl.addEventListener('mouseover', () => {
     const burgerLines = gsap.utils.toArray('.burger__line').forEach(line => {
@@ -20,5 +27,7 @@ burgerEl.addEventListener('mouseover', () => {
 })
 
 burgerEl.addEventListener('click', () => {
-    burgerTl.reversed(!burgerTl.reversed());
+    burgerTl.resume().reversed(!burgerTl.reversed());
+    navTl.resume().reversed(!navTl.reversed());
+    
 })
