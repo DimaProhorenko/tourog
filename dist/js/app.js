874000ddae2751d07382857bd7ea5e7b7de5efd7
DimaProhorenko <dima.study.web@gmail.com>
2023-03-30 01:06:40 +0300

    hero section without animation

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,23 @@
 const burgerEl = document.querySelector('.burger');
+const followBtn = document.querySelector('.header__follow');
 const duration = 0.3;
 
 
-// Tweens
+// Effects
+gsap.registerEffect({
+    name: 'overlaySlide',
+    effect: (targets, config) => {
+        console.log(targets);
+        const tl = gsap.timeline({ paused: true, reversed: true, ease: 'power4.inOut'});
+        tl.fromTo(targets[0], {xPercent: 100, duration }, {xPercent: 0, duration});
+        tl.fromTo(targets[1], {scaleX: 0, duration}, {scaleX: 1, duration});
+        return tl;
+    }
+})
 
-// Burger tween
+// Timelines
+
+// Burger Timeline
 const burgerTl = gsap.timeline({ paused: true, reversed: true })
     .to('.burger__line--top', { yPercent: 250, duration})
     .to('.burger__line--mid', {opacity: 0, duration}, '<')
@@ -14,11 +27,18 @@ const burgerTl = gsap.timeline({ paused: true, reversed: true })
     .to('.burger__line--bot', {rotation: -45, transformOrigin: 'center center', duration}, '<')
 
 
-// Nav Tween
-const navTl = gsap.timeline({ paused: true, reversed: true, ease: 'power4.inOut'})
-    .fromTo('.nav', {xPercent: 100, duration }, {xPercent: 0, duration})
-    .fromTo('.nav__overlay', {scaleX: 0, duration}, {scaleX: 1, duration})
-    .from('.nav__link', {y: -20, opacity: 0, duration, stagger: 0.1});
+// Nav Timeline
+// const navTl = gsap.timeline({ paused: true, reversed: true, ease: 'power4.inOut'})
+//     .fromTo('.nav', {xPercent: 100, duration }, {xPercent: 0, duration})
+//     .fromTo('.nav__overlay', {scaleX: 0, duration}, {scaleX: 1, duration})
+//     .from('.nav__link', {y: -20, opacity: 0, duration, stagger: 0.1});
+const navTl = gsap.effects.overlaySlide(['.nav', '.nav .overlay__bg']).from('.nav__link', {y: -20, opacity: 0, duration, stagger: 0.1});
+
+
+// Follow timeline
+const followTl = gsap.effects.overlaySlide(['.follow', '.follow .overlay__bg']).from('.follow__content', {opacity: 0, y: -50});
+
+
 
 burgerEl.addEventListener('mouseover', () => {
     const burgerLines = gsap.utils.toArray('.burger__line').forEach(line => {
@@ -30,4 +50,30 @@ burgerEl.addEventListener('click', () => {
     burgerTl.resume().reversed(!burgerTl.reversed());
     navTl.resume().reversed(!navTl.reversed());
     
+})
+
+followBtn.addEventListener('click', () => {
+    console.log('click');
+    followTl.resume().reversed(!followTl.reversed());
+})
+
+
+
+
+
+// Swiper
+const heroSwiper = new Swiper('.hero__swiper', {
+    loop: true,
+    effect: 'creative',
+    creativeEffect: {
+        prev: {translate: [0, 0, -800]},
+        next: {translate: ['100%', 0, 0]}
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 })
