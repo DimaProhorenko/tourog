f07604e84406a0e0e8adcf967c899a584e2c98a1
DimaProhorenko <dima.study.web@gmail.com>
2023-03-31 02:55:53 +0300

    intro scroll animation

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 const burgerEl = document.querySelector('.burger');
 const followBtn = document.querySelector('.header__follow');
 const duration = 0.3;
@@ -11,16 +13,147 @@ function animateHero() {
     return tl;
 }
 
+function animateTextOnScroll() {
+    // Create and insert spans for each letter
+    const animatedEls = document.querySelectorAll('.animated-text');
+    animatedEls.forEach(el => { 
+        const chars = el.innerText.split('');
+        console.log(chars);
+        el.innerHTML = '';
+        chars.forEach(char => {
+            const charSpan = document.createElement('span');
+            charSpan.innerHTML = char.replace(' ', '&nbsp;');
+            el.appendChild(charSpan);
+        })
+    })
+
+    animatedEls.forEach(el => {
+        const spans = el.querySelectorAll('span');
+        console.log(spans);
+        const parentSection = el.closest('.section');
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: el,
+                start: 'top bottom',
+                end: 'bottom+=100 bottom',
+                scrub: true,
+                
+            }
+
+        });
+        tl.from(spans, {
+            y: 10,
+            opacity: 0,
+            duration,
+            stagger: .1
+        })
+    })
+}
+
+
+function animateBeforeOnScroll() {
+    const animatedEls = document.querySelectorAll('.animate-before');
+    animatedEls.forEach(el => {
+        const parent = el.closest('.section');
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: parent,
+                start: 'top bottom',
+                end: 'bottom+=100 bottom',
+                scrub: true,
+                
+            }
+        });
+        tl.to(el, {
+            '--before-scale-x': 0,
+        })
+    })
+}
+
+function textRevealCircle() {
+    const animatedEls = document.querySelectorAll('.animate-circle');
+    animatedEls.forEach(el => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: el,
+                start: 'top+=150 bottom',
+                end: 'bottom+=400 bottom',
+                scrub: true,
+                
+            }
+        });
+        tl.to(el, {
+            "--clip": "100%",
+        })     
+    })
+}
+
+function textReveal(className) {
+    const animatedEls = document.querySelectorAll(className);
+    animatedEls.forEach(el => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: el,
+                start: 'top+=150 bottom',
+                end: 'bottom+=100 bottom',
+                scrub: true,
+                
+            }
+        });
+        tl.to(el, {
+            "--clip": "100%",
+        })     
+    })
+}
+
+function fadeRightOnScroll() {
+    const targets = gsap.utils.toArray('.fade-right');
+    targets.forEach(target => {
+        gsap.effects.fadeRight(target, {
+            scrollTrigger: {
+                trigger: target,
+                start: 'top+=150 bottom',
+                end: 'bottom+=100 bottom',
+                scrub: true,
+                
+            },
+            duration: 5
+        })
+        
+    })
+}
+
+function scale() {
+    const targets = gsap.utils.toArray('.scale');
+    targets.forEach(target => {
+        gsap.from(target, {
+            scale: 0,
+            scrollTrigger: {
+                trigger: target,
+                start: 'top+=150 bottom',
+                end: 'bottom+=100 bottom',
+                scrub: true,
+                
+            }
+        })
+    })
+}
 
 // Effects
 gsap.registerEffect({
     name: 'overlaySlide',
     effect: (targets, config) => {
-        console.log(targets);
         const tl = gsap.timeline({ paused: true, reversed: true, ease: 'power4.inOut'});
         tl.fromTo(targets[0], {xPercent: 100, duration }, {xPercent: 0, duration});
         tl.fromTo(targets[1], {scaleX: 0, duration}, {scaleX: 1, duration});
         return tl;
+    }
+})
+
+gsap.registerEffect({
+    name: 'fadeRight',
+    effect: (targets, config) => {
+       return gsap.from(targets, {x: '+=100', opacity: 0, scrollTrigger: config.scrollTrigger });
     }
 })
 
@@ -68,8 +201,11 @@ followBtn.addEventListener('click', () => {
 
 
 const heroTl = animateHero();
-
-
+animateTextOnScroll();
+// animateBeforeOnScroll();
+textRevealCircle();
+fadeRightOnScroll();
+scale();
 
 // Swiper
 const heroSwiper = new Swiper('.hero__swiper', {
